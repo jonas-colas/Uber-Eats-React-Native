ad1f69f08471f892d02b536c005e57df0495591e
jonascolas <jonas@fusemachines.com>
2021-12-19 16:29:54 -0300

    resto details

---
+++ components/restaurantDetail/MenuItems.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Divider } from 'react-native-elements';
+
+const MenuItems = ({food}) => {
+
+  return (
+    <View>
+      <>
+        <View style={styles.menuItems}>
+          <FoodInfo food={food} />
+          <FoodImage image={food.image} />
+        </View>
+        <Divider width={0.5} orientation="vertical"
+          style={{ borderBottomWidth: 1, marginHorizontal: 20 }}/>
+      </>
+    </View>
+  );
+};
+
+export default MenuItems;
+
+const FoodImage = ({image}) => (
+  <Image source={{uri: image}} style={styles.foodImage} />
+);
+
+const FoodInfo = (props) => (
+  <View style={styles.foodInfo}>
+    <Text style={styles.title}>{props.food.title}</Text>
+    <Text style={styles.desc}>{props.food.description}</Text>
+    <Text style={styles.price}>{props.food.price}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  menuItems:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 20,
+  },
+  foodImage: {
+    width: 100, 
+    height : 100,
+    borderRadius: 8,
+  },
+  foodInfo: {
+    width: 240,
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: '600',
+  },
+  desc: {
+
+  },
+  price: {},
+})
--- sreens/RestaurantDetail.js
+++ sreens/RestaurantDetail.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Divider } from 'react-native-elements';
 import About from '../components/restaurantDetail/About';
+import MenuItems from '../components/restaurantDetail/MenuItems';
+import { foods } from '../jsonDatas/menu';
+
 
 const info = {
   image: "https://thestayathomechef.com/wp-content/uploads/2017/08/Most-Amazing-Lasagna-2-e1574792735811.jpg",
@@ -15,6 +18,11 @@ const RestaurantDetail = () => {
     <View>
       <About info={info} />
       <Divider width={1.8} style={{marginVertical: 20}} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {foods.map((food, index) => (
+          <MenuItems food={food} key={index} /> 
+        ))}
+      </ScrollView>
     </View>
   );
 };
